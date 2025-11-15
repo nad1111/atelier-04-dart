3fef954c065a0acc012edc2696c171983b8d8a8b
nadir.fetis <n16635666@gmail.com>
2025-11-14 23:20:19 -0500

    exercice 05 q03,q05,q7,q09,

---
+++ exercice05.dart
@@ -0,0 +1,41 @@
+abstract class Forme {
+  double calculerAire();
+
+  void afficherMessage() {
+    print("Calcul de l'aire...");
+  }
+}
+
+class Cercle extends Forme {
+  final double rayon;
+
+  Cercle(this.rayon);
+
+  @override
+  double calculerAire() {
+    return 3.14 * rayon * rayon;
+  }
+}
+
+class Rectangle extends Forme {
+  final double longueur;
+  final double largeur;
+
+  Rectangle(this.longueur, this.largeur);
+
+  @override
+  double calculerAire() {
+    return longueur * largeur;
+  }
+}
+
+void main() {
+  var c = Cercle(5);
+  var r = Rectangle(4, 6);
+
+  c.afficherMessage();
+  print("Aire du cercle : ${c.calculerAire()}");
+
+  r.afficherMessage();
+  print("Aire du rectangle : ${r.calculerAire()}");
+}
